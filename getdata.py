05b2c8cb08b2bc4ff9cd538a8ad983a0022b2533
Tomo Bessho <tomobessho018@gmail.com>
2020-04-24 08:17:44 -0500

    tested trial with SIR with New York model

--- getdata.py
+++ getdata.py
@@ -2,51 +2,50 @@ import csv
 import os
 import pandas as pd
 
-frames = []
-for filename in os.listdir('COVID Analysis\\COVID-19\\csse_covid_19_data\\csse_covid_19_daily_reports'):
-    # making sure we are only reading the csv files
-    if os.path.splitext(filename)[1] == '.csv':
-        # print(os.path.splitext(filename)[0])
 
-        # reading in the data from the csv file
-        name = 'COVID Analysis\\COVID-19\\csse_covid_19_data\\csse_covid_19_daily_reports\\' + filename
-        readin_data = pd.read_csv(name)
+def getDataFromUS():
+    frames = []
+    for filename in os.listdir('COVID Analysis\\COVID-19\\csse_covid_19_data\\csse_covid_19_daily_reports'):
+        # making sure we are only reading the csv files
+        if os.path.splitext(filename)[1] == '.csv':
 
-        # creating the dataframe for this date
-        columns = ['Date', 'State', 'Confirmed', 'Deaths', 'Recovered']
-        day_data_formatted = pd.DataFrame(columns=columns)
+            # reading in the data from the csv file
+            name = 'COVID Analysis\\COVID-19\\csse_covid_19_data\\csse_covid_19_daily_reports\\' + filename
+            readin_data = pd.read_csv(name)
 
-        # obtaining the date
-        date = os.path.splitext(filename)[0]
-        
-        # getting the state (need to consider the two different formats of the csv *rolls eyes*)
-        if 'Province/State' in readin_data:
-            # data must be in the US
-            day_data_from_readin_in_us = readin_data[(readin_data['Country/Region']=='US')] 
+            # creating the dataframe for this date
+            columns = ['Date', 'State', 'Confirmed', 'Deaths', 'Recovered']
+            day_data_formatted = pd.DataFrame(columns=columns)
 
-            # creating an array for each information and putting them into one collective data frame
-            day_data_formatted['State'] =  day_data_from_readin_in_us['Province/State']
-            day_data_formatted['Confirmed'] =  day_data_from_readin_in_us['Confirmed']
-            day_data_formatted['Deaths'] = day_data_from_readin_in_us['Deaths']
-            day_data_formatted['Recovered'] = day_data_from_readin_in_us['Recovered']
-            day_data_formatted['Date'] = date
+            # obtaining the date
+            date = os.path.splitext(filename)[0]
+            
+            # getting the state (need to consider the two different formats of the csv *rolls eyes*)
+            if 'Province/State' in readin_data:
+                # data must be in the US
+                day_data_from_readin_in_us = readin_data[(readin_data['Country/Region']=='US')] 
 
-        elif 'Province_State' in readin_data:
-            # data must be in the US
-            day_data_from_readin_in_us = readin_data[(readin_data['Country_Region']=='US')] 
+                # creating an array for each information and putting them into one collective data frame
+                day_data_formatted['State'] =  day_data_from_readin_in_us['Province/State']
+                day_data_formatted['Confirmed'] =  day_data_from_readin_in_us['Confirmed']
+                day_data_formatted['Deaths'] = day_data_from_readin_in_us['Deaths']
+                day_data_formatted['Recovered'] = day_data_from_readin_in_us['Recovered']
+                day_data_formatted['Date'] = date
 
-            # creating an array for each information and putting them into one collective data frame
-            day_data_formatted['State'] =  day_data_from_readin_in_us['Province_State']
-            day_data_formatted['Confirmed'] =  day_data_from_readin_in_us['Confirmed']
-            day_data_formatted['Deaths'] = day_data_from_readin_in_us['Deaths']
-            day_data_formatted['Recovered'] = day_data_from_readin_in_us['Recovered']
-            day_data_formatted['Date'] = date
-        
-        frames.append(day_data_formatted)
+            elif 'Province_State' in readin_data:
+                # data must be in the US
+                day_data_from_readin_in_us = readin_data[(readin_data['Country_Region']=='US')] 
 
-covid_data = pd.concat(frames, ignore_index=True)
-print(covid_data)
+                # creating an array for each information and putting them into one collective data frame
+                day_data_formatted['State'] =  day_data_from_readin_in_us['Province_State']
+                day_data_formatted['Confirmed'] =  day_data_from_readin_in_us['Confirmed']
+                day_data_formatted['Deaths'] = day_data_from_readin_in_us['Deaths']
+                day_data_formatted['Recovered'] = day_data_from_readin_in_us['Recovered']
+                day_data_formatted['Date'] = date
+            
+            frames.append(day_data_formatted)
 
-covid_data.to_csv('test.csv', index=True)
+    covid_data = pd.concat(frames, ignore_index=True)
+    covid_data.to_csv('test.csv', index=True)
 
- 
+    return covid_data
--- sir.py
+++ sir.py
@@ -4,15 +4,18 @@ import matplotlib.pyplot as plt
 
 # This code was taken from: https://scipython.com/book/chapter-8-scipy/additional-examples/the-sir-epidemic-model/
 # All credit should go to this website
-
+# def SIR(N, I0, R0, beta, gamma):
 # Total population, N.
-N = 1000
+N = 19.45 * 10 ** 6
 # Initial number of infected and recovered individuals, I0 and R0.
-I0, R0 = 1, 0
+I0, R0 = 263292, 23887
+# Reproduction Rate (5.7 w/o social distancing, 1.5 w social distancing)
+r0 = 1.5
 # Everyone else, S0, is susceptible to infection initially.
-S0 = N - I0 - R0
+S0 = N - I0 - R0 
 # Contact rate, beta, and mean recovery rate, gamma, (in 1/days).
-beta, gamma = 0.2, 1./10 
+gamma =  1./14
+beta = gamma * r0
 # A grid of time points (in days)
 t = np.linspace(0, 160, 160)
 
@@ -33,12 +36,12 @@ S, I, R = ret.T
 # Plot the data on three separate curves for S(t), I(t) and R(t)
 fig = plt.figure(facecolor='w')
 ax = fig.add_subplot(111,  axisbelow=True)
-ax.plot(t, S/1000, 'b', alpha=0.5, lw=2, label='Susceptible')
+# ax.plot(t, S/1000, 'b', alpha=0.5, lw=2, label='Susceptible')
 ax.plot(t, I/1000, 'r', alpha=0.5, lw=2, label='Infected')
 ax.plot(t, R/1000, 'g', alpha=0.5, lw=2, label='Recovered with immunity')
 ax.set_xlabel('Time /days')
 ax.set_ylabel('Number (1000s)')
-ax.set_ylim(0,1.2)
+# ax.set_ylim(0,1.2)
 ax.yaxis.set_tick_params(length=0)
 ax.xaxis.set_tick_params(length=0)
 ax.grid(b=True, which='major', c='w', lw=2, ls='-')
